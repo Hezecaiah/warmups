3ea107db2e3ce7ac14d23e40fe4869fce0790a7b
Zeke <zeke.cyr@comcast.net>
2018-12-13 10:04:46 -0700

    reduce warmup completed

--- reduce_intro_warmup/main.js
+++ reduce_intro_warmup/main.js
@@ -1,22 +1,29 @@
 
 //add all elements together
 function sum(arr){
-
+  return arr.reduce(((acc, cur) => acc+cur),0)
 }
 
-// multiply all elements together
+//multiply all elements together
 function multiply(arr){
-
+  return arr.reduce(((acc,cur) => acc*cur),1)
 }
 
 //double each element using reduce
 function double(arr){
-
+  arr.reduce((acc,cur) => {
+    arr[acc] = cur*2
+    return acc+1
+  }, 0)
+  return arr
 }
 
-// //count the letters
+//count the letters
 function countLetters(str){
-
+  return [...str].reduce((acc,cur) => {
+    acc[cur] = (acc[cur] || 0) + 1
+    return acc
+  }, {})
 }
 
 module.exports = {
